6f210bf677e5bde48796e254b0acb7bb8568e6db
Steeven <82022828+steeven-th@users.noreply.github.com>
2023-12-03 14:21:39 +0100

    update

--- puppeteer.js
+++ puppeteer.js
@@ -1,4 +1,5 @@
 const puppeteer = require("puppeteer");
+const request = require("request");
 
 // Because everything in Puppeteer is asynchronous,
 // we wrap all of our code inside of an async IIFE
@@ -36,13 +37,127 @@ const puppeteer = require("puppeteer");
     // Call the function to scrape preparation steps
     datas.steps = await scrapeSteps(page);
 
-    // Afficher dans la console les datas
-    console.log(datas);
-
-    // Afficher en JSON
-    console.log(JSON.stringify(datas));
-
     await browser.close();
+
+    const fs = require('fs');
+    const PDFDocument = require('pdfkit');
+
+    // Create a PDF document
+    const doc = new PDFDocument({
+        layout: 'landscape', // Set layout to landscape
+        size: 'letter',       // Set paper size to letter
+    });
+
+    // Pipe the PDF content to a writable stream
+    const stream = fs.createWriteStream('output.pdf');
+    doc.pipe(stream);
+
+    doc.margins = { top: 0, bottom: 0, left: 0, right: 0 };
+
+    // Function to add a new page with content
+    function addPageWithContent(content) {
+        doc.addPage(); // Add a new page
+        doc.text(content, 50, 50); // Add content to the page
+    }
+
+    function downloadImage(url) {
+        return new Promise((resolve, reject) => {
+            request.get(url, { encoding: 'binary' }, (error, response, body) => {
+                if (!error && response.statusCode == 200) {
+                    const base64Image = Buffer.from(body, 'binary').toString('base64');
+                    resolve(`data:image/jpeg;base64,${base64Image}`);
+                } else {
+                    reject(error || 'Failed to download image');
+                }
+            });
+        });
+    }
+
+    async function addFirstPageContent(datas) {
+
+        // Add title to the first page
+        if (datas.title) {
+            doc.fontSize(18).text(datas.title, 340, 20, { width: 400, align: 'left', continued: false, indent: 0 });
+        }
+
+        // Add image to the first page
+        if (datas.image) {
+            try {
+                const imageData = await downloadImage(datas.image);
+                doc.image(imageData, 340, 60, { width: 400 });
+            } catch (error) {
+                console.error('Error:', error);
+            }
+        }
+
+        // Add information to the first page
+        if (datas.informations && datas.informations.length > 0) {
+
+            doc.strokeColor('#228800').lineWidth(2); // Red color
+
+            // Draw a rectangle around the text
+            doc.rect(20, 20, 300, 225).stroke();
+
+            doc.fontSize(14).text('Informations:', 25, 25, {continued: false, indent: 0});
+            doc.moveDown(); // Move down after the section title
+
+            datas.informations.forEach((info) => {
+                const key = Object.keys(info)[0];
+                const value = info[key];
+                doc.text(`${key}: ${value}`, {indent: 25});
+            });
+        }
+
+        // Add ingredients to the first page
+        if (datas.ingredients && datas.ingredients.length > 0) {
+
+            doc.strokeColor('#228800').lineWidth(2); // Red color
+
+            // Draw a rectangle around the text
+            doc.rect(20, 250, 300, 340).stroke();
+
+            doc.fontSize(14).text('Ingrédients:', 25, 255, {continued: false, indent: 0});
+            doc.moveDown(); // Move down after the section title
+
+            datas.ingredients.forEach((ingredient) => {
+                const key = Object.keys(ingredient)[0];
+                const value = ingredient[key];
+                doc.text(`${key}: ${value}`, {continued: false, indent: 25});
+            });
+        }
+
+        // Add ustensils to the first page
+        if (datas.ustensils && datas.ustensils.length > 0) {
+
+            doc.strokeColor('#228800').lineWidth(2); // Red color
+
+            // Draw a rectangle around the text
+            doc.rect(340, 350, 300, 240).stroke();
+
+            doc.fontSize(14).text('Ustensils:', 345, 355, {continued: true});
+            doc.moveDown().moveDown(); // Move down after the section title
+
+            datas.ustensils.forEach((ustensil) => {
+                doc.text(ustensil, {continued: false, indent: 25});
+            });
+
+            doc.moveDown(); // Move down after the ustensils section
+        }
+    }
+
+    // Add content to the first page
+    await addFirstPageContent(datas);
+
+    // Add content to the second page
+    addPageWithContent('Page 2 Content');
+
+    // Save the PDF
+    doc.end();
+
+    // Close the stream when the PDF generation is complete
+    stream.on('finish', () => {
+        console.log('PDF created successfully.');
+    });
 })();
 
 // Function to scrape Hero image
@@ -66,9 +181,15 @@ async function scrapeTitle(page) {
 async function scrapeInformations(page) {
 
     // ATTENTION : ne fonctionne que si { headless: false } est activé dans le launch de puppeteer
-    const buttonFor100g = await page.$('[aria-label="per-100g"]');
-    await buttonFor100g.click();
 
+        const buttonFor2Portions = await page.$('[aria-label="2"]');
+        await buttonFor2Portions.click();
+
+    // Si l'url est en .fr
+    if (page.url().includes('.fr')) {
+        const buttonFor100g = await page.$('[aria-label="per-100g"]');
+        await buttonFor100g.click();
+    }
     const elmtInformations = await page.$$('.kimgtP');
     const informations = [];
 
@@ -146,9 +267,4 @@ async function scrapeSteps(page) {
     }
 
     return steps;
-}
-
-// Function for convert get HTML from an element to text
-async function getTextFromElement(page, elementSelector) {
-    return await page.evaluate(elementSelector => elementSelector.outerHTML, elementSelector);
 }
